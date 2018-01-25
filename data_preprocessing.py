5fb284a74c9757f435d971d8b13f146ec1bf0a82
Zenon Foo <zenonfoowengzen@gmail.com>
2018-01-25 18:31:25 +0000

    More Data For BCC & Non-BCC Classification

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,36 +1,24 @@
 ### Data Extraction for BCC and non BCC ###
 import h5py
 import numpy as np
-import time
-import matplotlib.pyplot as plt
+import pandas as pd
 
-items = dict()
-testing = ['bcc', 'map_t']
-tissues_used = []
+pd.set_option('display.width',320)
 
-def preProcess(folder_name):
+folder_name = './RamanData/tissue_'
 
-    start = time.time()
-    BCCData = []
-    RamanData = []
-    tissues_used = []
+# Finding the unique keys for all the tissue files -  to identify callable keys when pre-processing data
+def findUniqueKeys(folder_name):
 
-    for x in range(3, 72):
+    unique_keys = []
+
+    for x in range(3,72):
 
         try:
             mat = h5py.File(folder_name + str(x) + '.mat')
 
-            # Checking just for BCC data in all the files
-            if testing[0] in list(mat.keys()):
-                bcc = np.array(mat[testing[0]])
-                RamanMap = np.array(mat[testing[1] + str(x)])
-
-                # Cheking for dimension mismatch
-                # If there is no dimension mismatch organise data to be raman input and BCC/NonBCC cell output
-                if bcc.shape[0] == RamanMap.shape[1] and bcc.shape[1] == RamanMap.shape[2]:
-                    BCCData.append(bcc)
-                    RamanData.append(RamanMap)
-                    tissues_used.append(x)
+            for item in list(mat.keys()):
+                unique_keys.append(item)
 
         except OSError:
             print(x, 'OSError')
@@ -38,12 +26,108 @@ def preProcess(folder_name):
         except KeyError:
             print(x, 'KeyError')
 
-    elapsed = time.time() - start
-    print(elapsed)
+    return np.unique(unique_keys)
 
-    return BCCData,RamanData,tissues_used
+# Returns 3 lists - BCC Label, Raman Data, Tissues Used
+# BCC Label - in the form of a list of 2D matrices
+# Raman Data - in the form of a list of 3D matrices
+def preProcess1(folder_name):
 
-def initalizeArray(bcc):
+    # Initializing variables
+    BCCData = []
+    RamanData = []
+    tissues_used = []
+
+    # Keys in the dataset
+    testing = ['bcc']
+    map = 'map_t'
+
+    for x in range(3, 72):
+
+        try:
+            # Opening file and obtaining keys
+            mat = h5py.File(folder_name + str(x) + '.mat')
+            keys = list(mat.keys())
+
+            # Checking just for BCC data in all the files
+            if testing[0] in keys:
+                bcc = np.array(mat[testing[0]])
+                RamanMap = np.array(mat[map + str(x)])
+
+            else:
+                continue
+
+            # Cheking for dimension mismatch
+            # If there is no dimension mismatch organise data to be raman input and BCC/NonBCC cell output
+            if bcc.shape[0] == RamanMap.shape[1] and bcc.shape[1] == RamanMap.shape[2]:
+                BCCData.append(bcc)
+                RamanData.append(RamanMap)
+                tissues_used.append(x)
+
+        except OSError:
+            print(x, 'OSError')
+
+        except KeyError:
+            print(x, 'KeyError')
+
+        except IndexError:
+            print(x, 'IndexError')
+
+    return BCCData, RamanData, tissues_used
+
+
+# This is for the tissues with multiple tasks
+# Returns 3 lists - BCC Label, Raman Data, Tissues Used
+# BCC Label - in the form of a list of 2D matrices
+# Raman Data - in the form of a list of 3D matrices
+def preProcess2(folder_name):
+    # Initializing variables
+    BCCData = []
+    RamanData = []
+    tissues_used = []
+
+    # Keys in the dataset
+    testing = ['bcc']
+    map = 'map_t'
+
+    for x in range(3, 72):
+
+        for y in range(1,3):
+
+            try:
+                # Opening file and obtaining keys
+                mat = h5py.File(folder_name + str(x) + '_' + str(y) +'.mat')
+                keys = list(mat.keys())
+
+                # Checking just for BCC data in all the files
+                if testing[0] in keys:
+                    bcc = np.array(mat[testing[0]])
+                    RamanMap = np.array(mat[map + str(x) + '_' + str(y)])
+
+                else:
+                    continue
+
+                # Cheking for dimension mismatch
+                # If there is no dimension mismatch organise data to be raman input and BCC/NonBCC cell output
+                if bcc.shape[0] == RamanMap.shape[1] and bcc.shape[1] == RamanMap.shape[2]:
+                    BCCData.append(bcc)
+                    RamanData.append(RamanMap)
+                    tissues_used.append(str(x) + '_' + str(y))
+
+            except OSError:
+                print(x, 'OSError')
+
+            except KeyError:
+                print(x, 'KeyError')
+
+            except IndexError:
+                print(x, 'IndexError')
+
+    return BCCData, RamanData, tissues_used
+
+
+# Looks at the dimensions of each tissue data to initalize matrix to store
+def initalizeMatrix(bcc):
     row_size = 0
 
     for item in range(len(bcc)):
@@ -51,9 +135,10 @@ def initalizeArray(bcc):
 
     return row_size
 
+#
 def organiseData(bcc,raman):
 
-    row_size = initalizeArray(bcc)
+    row_size = initalizeMatrix(bcc)
     data = np.zeros((row_size,1025))
     counter = 0
 
@@ -66,5 +151,10 @@ def organiseData(bcc,raman):
                 counter += 1
 
     return data
+
+
+# Saving Data
+np.save('BCC_Data_2',data)
+np.save('Tissues_Used_2',tissue_used)
 
 
--- neural_network.py
+++ neural_network.py
@@ -1,53 +1,93 @@
 ## Neural Network fo classifying BCC and non BCC cells
 
-import pandas as pd
 import numpy as np
 
+folder_name = 'BCC&NoBCC_Classification/BCC_Data_2.npy'
+
 # Importing dataset
-data = np.load('BCC&NoBCC_Classification/BCC_Data.npy')
-X = np.array([row[:-1] for row in data])
-y = np.array([row[-1] for row in data])
+def import_data(folder_name):
 
-# Splitting dataset into training and test set
-from sklearn.model_selection import train_test_split
-# random_state is property that if you don't set it everytime you run the function there is a different outcome
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.1)
+    data = np.load(folder_name)
+    X = data[:,:-1]
+    y = data[:,-1]
 
-# Feature Scaling
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
+    return data,X,y
 
-# Making the ANN
-from keras.models import Sequential
-from keras.layers import Dense
+# Seperating data - with the main goal of normalizing the dataset w.r.t only the non-bcc raman data
+def seperate(data):
 
-# Initializing the ANN
-classifier = Sequential()
+    # Initializing matrix so store the bcc data and non bcc data
+    row_data = data.shape[0]
+    count_bcc = [1 for item in range(row_data) if data[item,-1] == 1]
+    sum_bcc = sum(count_bcc)
+    bcc_store = np.empty([sum_bcc,1025])
+    non_bcc_store = np.empty([row_data-sum_bcc,1025])
 
-# Adding to the input layer and the first hidden layer
-# kernel_initializer refers to inital weights
-classifier.add(Dense(units=100, kernel_initializer='uniform', activation='sigmoid', input_dim=1024))
+    bcc_counter = 0
+    non_bcc_counter = 0
 
-# Adding second layer
-classifier.add(Dense(units=100, kernel_initializer='uniform', activation='sigmoid'))
+    for item in range(row_data):
 
-# Adding output layer
-classifier.add(Dense(units=1, kernel_initializer='uniform', activation='sigmoid'))
+        if data[item,-1] == 1:
+            bcc_store[bcc_counter,:] = data[item,:]
+            bcc_counter += 1
 
-# Compiling the ANN
-# optimizer: algorithm you want to use to find the optimal set of weights, adam is stochastic gradient descent
-# loss: loss function - binary_crossentropy is the logarithmic loss function
-classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+        else:
+            non_bcc_store[non_bcc_counter,:] = data[item,:]
+            non_bcc_counter += 1
 
-# Fitting classifier to training set
-classifier.fit(X_train, y_train, batch_size=100, epochs=100)
+    return bcc_store[:,:-1],non_bcc_store[:,:-1]
 
-# Predicting the Test set results
-y_pred = classifier.predict(X_test)
-y_pred = (y_pred>0.5)
+# Splitting dataset into training and test set & Feature Scaling
+def split_normalize(X,y):
 
-# Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_test, y_pred)
+    # Splitting dataset into training and test set
+    from sklearn.model_selection import train_test_split
+    # random_state is property that if you don't set it everytime you run the function there is a different outcome
+    X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.1)
+
+    # Feature Scaling - using non-bcc Raman spectra as a base
+    from sklearn.preprocessing import StandardScaler
+    sc = StandardScaler()
+    X_train = sc.fit_transform(X_train)
+    X_test = sc.transform(X_test)
+
+    return X_train, X_test, y_train, y_test
+
+# Running ANN algorithm
+def neural_network(X_train, X_test, y_train, y_test):
+
+    # Making the ANN
+    from keras.models import Sequential
+    from keras.layers import Dense
+
+    # Initializing the ANN
+    classifier = Sequential()
+
+    # Adding to the input layer and the first hidden layer
+    # kernel_initializer refers to inital weights
+    classifier.add(Dense(units=50, kernel_initializer='uniform', activation='sigmoid', input_dim=1024))
+
+    # Adding second layer
+    classifier.add(Dense(units=50, kernel_initializer='uniform', activation='sigmoid'))
+
+    # Adding output layer
+    classifier.add(Dense(units=1, kernel_initializer='uniform', activation='sigmoid'))
+
+    # Compiling the ANN
+    # optimizer: algorithm you want to use to find the optimal set of weights, adam is stochastic gradient descent
+    # loss: loss function - binary_crossentropy is the logarithmic loss function
+    classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    # Fitting classifier to training set
+    classifier.fit(X_train, y_train, batch_size=100, epochs=10)
+
+    # Predicting the Test set results
+    y_pred = classifier.predict(X_test)
+    y_pred = (y_pred>0.5)
+
+    # Making the Confusion Matrix
+    from sklearn.metrics import confusion_matrix
+    cm = confusion_matrix(y_test, y_pred)
+
+    return cm
